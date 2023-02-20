9696f6f885b2e2238cac0152eae13d4e5a2e87c8
Egor Talovikov <egtalovikov@gmail.com>
2023-02-20 23:04:20 +0500

    hotfix: исправил замечания линтера

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
-const { NODE_ENV, JWT_SECRET, PORT, MONGO_URL } = process.env;
+const {
+  NODE_ENV, JWT_SECRET, PORT, MONGO_URL,
+} = process.env;
 
-module.exports = { PORT, JWT_SECRET, NODE_ENV, MONGO_URL };
+module.exports = {
+  PORT, JWT_SECRET, NODE_ENV, MONGO_URL,
+};
