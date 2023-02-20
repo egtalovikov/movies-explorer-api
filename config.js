2ab2787fca72289ab90f9038dd186bb6afd371a6
Egor Talovikov <egtalovikov@gmail.com>
2023-02-20 22:52:12 +0500

    feat: добавил mongo_url в config

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const { NODE_ENV, JWT_SECRET, PORT } = process.env;
+const { NODE_ENV, JWT_SECRET, PORT, MONGO_URL } = process.env;
 
-module.exports = { PORT, JWT_SECRET, NODE_ENV };
+module.exports = { PORT, JWT_SECRET, NODE_ENV, MONGO_URL };
